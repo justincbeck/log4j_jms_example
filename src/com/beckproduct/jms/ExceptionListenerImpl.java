b096b12844bac3502376c9a2e7fed4bff928c551
jbeck <jbeck@17cc8b5f-324b-4e7c-ae86-03e2ff4148b7>
2008-01-12 04:33:20 +0000

    Ditched the datasource.
    git-svn-id: file:///svnroot/log4j-jms-example/trunk@16 17cc8b5f-324b-4e7c-ae86-03e2ff4148b7

--- src/com/beckproduct/jms/ExceptionListenerImpl.java
+++ src/com/beckproduct/jms/ExceptionListenerImpl.java
@@ -3,7 +3,6 @@ package com.beckproduct.jms;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
-import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
@@ -12,11 +11,6 @@ public class ExceptionListenerImpl implements MessageListener
 {
     private Logger logger = Logger.getLogger(this.getClass());
 
-    /**
-     * The dataSource for this class.
-     */
-    private DataSource dataSource;
-
     public void onMessage(Message message)
     {
         try
@@ -24,20 +18,11 @@ public class ExceptionListenerImpl implements MessageListener
             ObjectMessage m = (ObjectMessage) message;
             LoggingEvent e = (LoggingEvent) m.getObject();
             
-            logger.info("Kaboom!", e.getThrowableInformation().getThrowable());
+            logger.info(e.getRenderedMessage());
         }
         catch (Exception e)
         {
             logger.info("Boom!", e);
         }
-    }
-
-    /**
-     * @param dataSource
-     *            the dataSource to set
-     */
-    public void setDataSource(DataSource dataSource)
-    {
-        this.dataSource = dataSource;
     }
 }
