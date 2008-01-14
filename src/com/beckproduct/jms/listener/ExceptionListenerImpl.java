2c68110872a4087d3f0156a9f32f50a5c5a900c8
jbeck <jbeck@17cc8b5f-324b-4e7c-ae86-03e2ff4148b7>
2008-01-14 21:14:01 +0000

    Added persistence to listener.
    git-svn-id: file:///svnroot/log4j-jms-example/trunk@43 17cc8b5f-324b-4e7c-ae86-03e2ff4148b7

--- src/com/beckproduct/jms/listener/ExceptionListenerImpl.java
+++ src/com/beckproduct/jms/listener/ExceptionListenerImpl.java
@@ -9,16 +9,17 @@ import javax.jms.ObjectMessage;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.beckproduct.domain.LogEntry;
-import com.beckproduct.repository.ILogEntryRepository;
 
-public class ExceptionListenerImpl implements MessageListener
+public class ExceptionListenerImpl extends HibernateDaoSupport implements MessageListener
 {
     private Logger logger = Logger.getLogger(this.getClass());
     
-//    private ILogEntryRepository repository;
-
+    private PlatformTransactionManager transactionManager;
+    
     public void onMessage(Message jmsMessage)
     {
         try
@@ -29,30 +30,36 @@ public class ExceptionListenerImpl implements MessageListener
             LogEntry entry = new LogEntry();
             entry.setLogLevel(loggingEvent.getLevel().toString());
             entry.setHostName(InetAddress.getLocalHost().getHostName());
-            entry.setStackTrace(loggingEvent.getThrowableStrRep());
+            entry.setStackTrace(loggingEvent);
             entry.setDate(new Date(loggingEvent.timeStamp));
             
-//            repository.create(entry);
+            this.create(entry);
         }
         catch (Exception e)
         {
             logger.info("Boom!", e);
         }
     }
+    
+    private void create(LogEntry entry)
+    {
+        getHibernateTemplate().saveOrUpdate(entry);
+        getHibernateTemplate().flush();
+    }
 
-//    /**
-//     * @return the repository
-//     */
-//    public ILogEntryRepository getRepository()
-//    {
-//        return repository;
-//    }
-//
-//    /**
-//     * @param repository the repository to set
-//     */
-//    public void setRepository(ILogEntryRepository repository)
-//    {
-//        this.repository = repository;
-//    }
+    /**
+     * @return the transactionManager
+     */
+    public PlatformTransactionManager getTransactionManager()
+    {
+        return transactionManager;
+    }
+
+    /**
+     * @param transactionManager the transactionManager to set
+     */
+    public void setTransactionManager(PlatformTransactionManager transactionManager)
+    {
+        this.transactionManager = transactionManager;
+    }
 }
