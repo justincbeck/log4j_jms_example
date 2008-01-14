34d9fef1d3afce786aa387c742e65f2b2adb2a17
jbeck <jbeck@17cc8b5f-324b-4e7c-ae86-03e2ff4148b7>
2008-01-14 17:32:59 +0000

    Now populates a domain object with all the data we want.
    git-svn-id: file:///svnroot/log4j-jms-example/trunk@34 17cc8b5f-324b-4e7c-ae86-03e2ff4148b7

--- src/com/beckproduct/jms/listener/ExceptionListenerImpl.java
+++ src/com/beckproduct/jms/listener/ExceptionListenerImpl.java
@@ -1,5 +1,8 @@
 package com.beckproduct.jms.listener;
 
+import java.net.InetAddress;
+import java.util.Date;
+
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
@@ -7,22 +10,49 @@ import javax.jms.ObjectMessage;
 import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
 
+import com.beckproduct.domain.LogEntry;
+import com.beckproduct.repository.ILogEntryRepository;
+
 public class ExceptionListenerImpl implements MessageListener
 {
     private Logger logger = Logger.getLogger(this.getClass());
+    
+//    private ILogEntryRepository repository;
 
-    public void onMessage(Message message)
+    public void onMessage(Message jmsMessage)
     {
         try
         {
-            ObjectMessage m = (ObjectMessage) message;
-            LoggingEvent e = (LoggingEvent) m.getObject();
+            ObjectMessage objectMessage = (ObjectMessage) jmsMessage;
+            LoggingEvent loggingEvent = (LoggingEvent) objectMessage.getObject();
             
-            logger.info(e.getRenderedMessage());
+            LogEntry entry = new LogEntry();
+            entry.setLogLevel(loggingEvent.getLevel().toString());
+            entry.setHostName(InetAddress.getLocalHost().getHostName());
+            entry.setStackTrace(loggingEvent.getThrowableStrRep());
+            entry.setDate(new Date(loggingEvent.timeStamp));
+            
+//            repository.create(entry);
         }
         catch (Exception e)
         {
             logger.info("Boom!", e);
         }
     }
+
+//    /**
+//     * @return the repository
+//     */
+//    public ILogEntryRepository getRepository()
+//    {
+//        return repository;
+//    }
+//
+//    /**
+//     * @param repository the repository to set
+//     */
+//    public void setRepository(ILogEntryRepository repository)
+//    {
+//        this.repository = repository;
+//    }
 }
